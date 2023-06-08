d00c787faa86a242a2330c09012f53b86a1b7449
D3nam <rahl302000@gmail.com>
2023-06-08 18:19:04 -0400

    feat: ✨ Added Model for Learning
    Added Model for Learning including Course, Units, Articles and Videos additionally added one record of each to Database

---
+++ server/src/models/Learning/course.ts
@@ -0,0 +1,21 @@
+import { model, Schema } from "mongoose"
+import { Course } from "./types/interface";
+
+const CourseSchema = new Schema<any>({
+  name: {
+      type: "String",
+      required: true,
+  },
+  icon: {
+    type: String,
+    required: true
+  },
+  units: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Unit'
+  }],
+
+});
+
+const modelCourse = model<Course>("Course", CourseSchema)
+export default modelCourse;
---
+++ server/src/models/Learning/video.ts
@@ -0,0 +1,34 @@
+import { model, Schema } from "mongoose"
+import { Video } from "./types/interface";
+
+const VideoSchema = new Schema({
+  title : {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+    immutable: true
+  },
+  updatedAt: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+  }
+})
+
+const modelVideo = model<Video>("Video", VideoSchema)
+export default modelVideo;
