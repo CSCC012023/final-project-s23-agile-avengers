f8060eda7d168a0c3d70e4cfaa2cc5cedfc9a323
Aryan Thakur <aryan.documents@yahoo.com>
2023-06-09 13:33:53 -0400

    Set up

---
+++ client/app/learning/page.tsx
@@ -0,0 +1,7 @@
+export default function AllCourses() {
+  return (
+    <>
+      <h1>Hello Broski</h1>
+    </>
+  )
+}
---
+++ client/components/AllCourses.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React, { useEffect, useState } from 'react';
+
+import {
+   Flex
+} from '@chakra-ui/react'
+
+const ListCourses = () => {
+
+    const [courses, setCourses] = useState<Array<any>>([])
+
+    return (
+        <>
+        </>
+    );
+
+};
+
+export default ListCourses;
