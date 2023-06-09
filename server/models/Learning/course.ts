d4fe4120ff0f5022c8b9d688d5f8c106b5f3a56f
D3nam <rahl302000@gmail.com>
2023-06-09 18:30:02 -0400

    feat: ✨ Db Schema for Learning epic

---
+++ server/models/Learning/course.ts
@@ -0,0 +1,24 @@
+import { model, Schema } from 'mongoose';
+import { Course } from '../../types/learning';
+/*
+The course model from our database
+*/
+const CourseSchema = new Schema<Course>({
+  name: {
+    type: 'String',
+    required: true,
+  },
+  icon: {
+    type: String,
+    required: true,
+  },
+  units: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Unit',
+    },
+  ],
+});
+
+const modelCourse = model<Course>('Course', CourseSchema);
+export default modelCourse;
