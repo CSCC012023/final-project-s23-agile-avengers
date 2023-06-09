aa1319737057cb39bad5e6d4fc26e073ed00e218
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2023-06-08 23:46:05 -0400

    feat: 🔍️ Added Site Metadata

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,5 +1,12 @@
 import { Providers } from "./providers";
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: 'FinLearn',
+	description: 'A Financial Learning Site'
+};
+
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
