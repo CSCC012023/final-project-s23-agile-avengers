2c793e7b10b67e8cb0131a8cd9e0ab7b0a892c51
D3nam <rahl302000@gmail.com>
2023-06-08 20:25:51 -0400

    feat: 🚧 Initial work for Navbar

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ChakraProvider } from '@chakra-ui/react'
+import Navbar from '@/components/Navigation-Bar/Navbar';
 
 export default function RootLayout({
   children,
@@ -10,7 +11,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <ChakraProvider>
+          <Navbar></Navbar>
           {children}
         </ChakraProvider>
       </body>
