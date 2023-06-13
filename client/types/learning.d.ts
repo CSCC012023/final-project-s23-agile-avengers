44083ab8ec16a825d5ccb4fb48a1eeb19b33a566
Shawn Santhoshgeorge <32755895+ShawnGeorge03@users.noreply.github.com>
2023-06-13 14:17:20 -0400

    refactor: 🏷️ Updated Learning and Nav Link Types

---
+++ client/types/learning.d.ts
@@ -0,0 +1,33 @@
+/*
+Types needed for Learning models
+*/
+
+export interface Course {
+  name: String;
+  icon: String;
+  units: Array<Unit>
+};
+
+export interface Unit {
+  name: String;
+  createdAt: Date;
+  updatedAt: Date;
+  content: Array<Article | Video>
+};
+
+export interface Article {
+  title: String;
+  createdAt: Date;
+  updatedAt: Date;
+  image?: String;
+  author: String;
+};
+
+export interface Video {
+  title: String;
+  createdAt: Date;
+  updatedAt: Date;
+  link: String;
+  author: String;
+  description?: String;
+};
