4ca4fa045db232ff3ae8409ba9216d8e4e67c725
D3nam <rahl302000@gmail.com>
2023-06-15 15:02:04 -0400

    feat: 💄 Improved UI for Dashboard-Learning Page

--- client/components/Dashboard-Learning/Sidebar.tsx
+++ client/components/Dashboard-Learning/Sidebar.tsx
@@ -6,17 +6,38 @@ import {
   Box,
   Heading,
   CardBody,
-  Text,
+  HStack,
   StackDivider,
   Stack,
   Divider,
+  Icon,
 } from '@chakra-ui/react';
 import styles from '../../styles/components/sidebar.module.scss';
 import { Course } from '@/types/learning';
+import { AiFillCaretRight } from 'react-icons/ai';
+
 type SideBarProps = {
   courses: Course[];
+  selectedCourse: Course | undefined;
+  setSelectedCourse: React.Dispatch<React.SetStateAction<Course | undefined>>;
 };
-const Sidebar = ({ courses }: SideBarProps) => {
+
+const Sidebar = ({
+  courses,
+  selectedCourse,
+  setSelectedCourse,
+}: SideBarProps) => {
+  const findCourseByName = (name: string) => {
+    for (let i = 0; i < courses.length; i++) {
+      if (courses[i].name == name) {
+        return courses[i];
+      }
+    }
+    return courses[0];
+  };
+  const handleClick = (e: any) => {
+    setSelectedCourse(findCourseByName((e.target.parentElement as any).id));
+  };
   return (
     <Card bg="brand.gray">
       <CardHeader>
@@ -28,18 +49,29 @@ const Sidebar = ({ courses }: SideBarProps) => {
           divider={<StackDivider color="brand.black" />}
           spacing="4">
           {courses.map((course) => (
-            <Box key={course.name as string}>
+            <HStack
+              key={course.name.toString()}
+              id={course.name.toString()}
+              onClick={handleClick}
+              textColor={
+                selectedCourse && selectedCourse.name == course.name
+                  ? 'brand.blue'
+                  : 'brand.black'
+              }>
               <Heading
                 size="xs"
+                display={'inline'}
                 textTransform="uppercase">
                 {course.name}
               </Heading>
-              <Text
-                pt="2"
-                fontSize="sm">
-                Hello World
-              </Text>
-            </Box>
+              {selectedCourse && selectedCourse.name == course.name && (
+                <Icon
+                  as={AiFillCaretRight}
+                  ml={2}
+                  boxSize={'m'}
+                />
+              )}
+            </HStack>
           ))}
         </Stack>
       </CardBody>
--- client/types/components/Dashboard-Learning/types.d.ts
+++ client/types/components/Dashboard-Learning/types.d.ts
@@ -2,5 +2,5 @@ export type Unit = {
   name: String;
   content: {
     name: string;
-  }[];
+  };
 };
