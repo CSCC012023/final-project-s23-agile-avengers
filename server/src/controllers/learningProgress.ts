2e8578fa2ee880527e7b437195aea58d526c805b
D3nam <rahl302000@gmail.com>
2023-07-04 14:38:19 -0400

    fix: ♻️ Refactor code to fix linter warnings

--- server/src/controllers/learningProgress.ts
+++ server/src/controllers/learningProgress.ts
@@ -1,23 +1,24 @@
 import { Request, Response } from 'express';
 import { modelUser } from '../models/Account/user';
 import { modelProgress } from '../models/Learning/progress';
-import modelUnit from '../models/Learning/unit';
 
+/*
+Endpoint to get a user's learning progress from their userId
+*/
 export const getLearningProgress = async (req: Request, res: Response) => {
-  if (!req.query.userID) {
+  if (!req.query.userID)
     return res.status(400).json({
       message: 'Please provide userID',
     });
-  }
 
   const user = await modelUser.findOne({
     userID: req.query.userID,
   });
-  if (!user) {
+  if (!user)
     return res.status(400).json({
       message: 'User not found with the given userID',
     });
-  }
+
   const learningProgress = await modelProgress
     .findOne({
       userID: user._id,
@@ -38,8 +39,6 @@ export const getLearningProgress = async (req: Request, res: Response) => {
     })
     .exec();
 
-  if (!learningProgress) {
-    return res.send({});
-  }
+  if (!learningProgress) return res.send({});
   res.send(learningProgress);
 };
