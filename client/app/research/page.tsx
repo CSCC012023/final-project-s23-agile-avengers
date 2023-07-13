a3cd368a6ec2febb0070788a3d9a090de9b4fdf4
Adam Badar <adam.badar@mail.utoronto.ca>
2023-07-13 19:28:41 -0400

    feat: ✨ added stock screener to the research page

--- client/app/research/page.tsx
+++ client/app/research/page.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import { Heading } from '@chakra-ui/react';
 import {
   CopyrightStyles,
   MiniChart,
+  Screener,
   SingleTicker,
 } from 'react-ts-tradingview-widgets';
 
@@ -22,6 +24,19 @@ export default function ResearchPage() {
         colorTheme="dark"
         copyrightStyles={styles}
         width="100%"></SingleTicker>
+      <Heading
+        mb="1"
+        mt="3"
+        size="md"
+        w="100%">
+        STOCK SCREENER
+      </Heading>
+      <Screener
+        colorTheme="dark"
+        defaultScreen="most_capitalized"
+        height={300}
+        market="america"
+        width="100%"></Screener>
     </>
   );
 }
