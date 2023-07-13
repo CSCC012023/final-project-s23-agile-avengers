1dc71df521ea258f31d89c639779b330700ff187
ShawnGeorge03 <ShawnGeorge03@users.noreply.github.com>
2023-07-13 05:41:04 +0000

    style: 🎨 Formatted Client & Server Code

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,14 +1,13 @@
 import { connect } from 'mongoose';
 import { load } from 'ts-dotenv';
 
-
 /**
  * Initiates the connection to MongoDB Atlas
  *
  * @param {string} MONGO_URI - URI of Cluster
  */
 export const connectDB = async () => {
-  const { MONGO_URI } = load({MONGO_URI: String})
+  const { MONGO_URI } = load({ MONGO_URI: String });
   try {
     await connect(MONGO_URI);
     console.info('MongoDB is connected 🟢');
