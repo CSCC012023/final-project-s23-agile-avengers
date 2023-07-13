e69faa99affc3617be924f4ab3e6655e106640f8
ShawnGeorge03 <32755895+ShawnGeorge03@users.noreply.github.com>
2023-07-13 13:09:05 -0400

    feat: 🗃️ added custom `toJSON` for Learning Schema
    FIN-122

--- server/src/models/Learning/unit.ts
+++ server/src/models/Learning/unit.ts
@@ -4,29 +4,42 @@ import { Unit } from '../../types/learning';
 /*
 The unit model from our database
 */
-const UnitSchema = new Schema<Unit>({
-  name: {
-    type: String,
-    required: true,
+const UnitSchema = new Schema<Unit>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    createdAt: {
+      type: Date,
+      required: true,
+      default: Date.now(),
+      immutable: true,
+    },
+    updatedAt: {
+      type: Date,
+      required: true,
+      default: Date.now(),
+    },
+    content: [{ type: Schema.Types.Mixed }],
   },
-  slug: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  createdAt: {
-    type: Date,
-    required: true,
-    default: Date.now(),
-    immutable: true,
-  },
-  updatedAt: {
-    type: Date,
-    required: true,
-    default: Date.now(),
-  },
-  content: [{ type: Schema.Types.Mixed }],
-});
+  {
+    toJSON: {
+      // This function returns a JSON without id, __v
+      transform: (doc, ret) => {
+        delete ret.id;
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+      },
+    },
+  }
+);
 
 const modelUnit = model<Unit>('Unit', UnitSchema);
 export default modelUnit;
