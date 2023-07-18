6ad072c6ce8870e4c336ad50e359c178ac2d9aa8
ShawnGeorge03 <32755895+ShawnGeorge03@users.noreply.github.com>
2023-07-18 13:27:02 -0400

    style: 💄 updated `ResearchPage` styling

--- client/app/research/page.tsx
+++ client/app/research/page.tsx
@@ -3,28 +3,30 @@
 import { Heading } from '@chakra-ui/react';
 import { CopyrightStyles, Screener } from 'react-ts-tradingview-widgets';
 
+import styles from '@/styles/pages/Research.module.scss';
+
 export default function ResearchPage() {
-  const styles: CopyrightStyles = {
-    parent: {
-      display: 'none',
-    },
-  };
+  const tradingViewStyles: CopyrightStyles = { parent: { display: 'none' } };
+
+  const { container } = styles;
+
   return (
-    <>
+    <div className={container}>
       <Heading
         mb="1"
         mt="3"
         size="md"
         w="100%">
-        STOCK SCREENER
+        Stock Screener
       </Heading>
+
       <Screener
         colorTheme="light"
-        copyrightStyles={styles}
+        copyrightStyles={tradingViewStyles}
         defaultScreen="most_capitalized"
-        height={300}
         market="america"
-        width="100%"></Screener>
-    </>
+        width="100%"
+      />
+    </div>
   );
 }
