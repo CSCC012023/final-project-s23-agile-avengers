4abe5c4562539a54cfd0029c00fe1132b2c373d7
ShawnGeorge03 <32755895+ShawnGeorge03@users.noreply.github.com>
2023-07-18 13:40:52 -0400

    feat: ✨ created `ResearchInfoPage`
    FIN-33

---
+++ client/app/research/info/page.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { Heading } from '@chakra-ui/react';
+import { useRouter } from 'next/navigation';
+
+import styles from '@/styles/pages/ResearchInfo.module.scss';
+
+type ResearchInfoProps = {
+  searchParams: {
+    tvwidgetsymbol: string;
+  };
+};
+
+export default function ResearchInfoPage({ searchParams }: ResearchInfoProps) {
+  const { container } = styles;
+
+  const router = useRouter();
+
+  const parseSymbol = (tvwidgetsymbol: string) => {
+    try {
+      return !tvwidgetsymbol.includes(':')
+        ? tvwidgetsymbol
+        : tvwidgetsymbol.split(':')[1];
+    } catch (error) {
+      router.push('/research');
+    }
+  };
+
+  const symbol = parseSymbol(searchParams.tvwidgetsymbol);
+
+  return (
+    <>
+      <div className={container}>
+        <Heading>{symbol}</Heading>
+      </div>
+    </>
+  );
+}
