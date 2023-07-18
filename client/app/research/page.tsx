3450637cc052d95324a49995c76d4d0bbc895c81
ShawnGeorge03 <32755895+ShawnGeorge03@users.noreply.github.com>
2023-07-18 13:32:10 -0400

    feat: ✨ added `SymbolSearch` to `ResearchPage`
    FIN-33

--- client/app/research/page.tsx
+++ client/app/research/page.tsx
@@ -3,15 +3,25 @@
 import { Heading } from '@chakra-ui/react';
 import { CopyrightStyles, Screener } from 'react-ts-tradingview-widgets';
 
+import SymbolSearch from '@/components/SymbolSearch';
+
 import styles from '@/styles/pages/Research.module.scss';
 
 export default function ResearchPage() {
   const tradingViewStyles: CopyrightStyles = { parent: { display: 'none' } };
 
-  const { container } = styles;
+  const { container, searchWrapper } = styles;
 
   return (
     <div className={container}>
+      <Heading
+        as="h1"
+        size="xl">
+        Research
+      </Heading>
+      <div className={searchWrapper}>
+        <SymbolSearch callback={(symbol: string) => console.log(symbol)} />
+      </div>
       <Heading
         mb="1"
         mt="3"
