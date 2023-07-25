5f143d779249c9333b897859095e5af1a37fc2cf
ShawnGeorge03 <32755895+ShawnGeorge03@users.noreply.github.com>
2023-07-25 12:09:54 -0400

    feat: 🗃️ added custom `toJSON` for Trading Schema
    FIN-119

--- server/src/models/Trading/account.ts
+++ server/src/models/Trading/account.ts
@@ -2,26 +2,39 @@ import { Schema, model } from 'mongoose';
 
 import { Account } from '../../types/trading';
 
-const AccountSchema = new Schema<Account>({
-  userID: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-    immutable: true,
-    unique: true,
+const AccountSchema = new Schema<Account>(
+  {
+    userID: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      immutable: true,
+      unique: true,
+    },
+    cash: {
+      type: Number,
+      min: 0,
+      default: 100000,
+      required: true,
+    },
+    holdings: {
+      type: Number,
+      min: 0,
+      required: true,
+    },
   },
-  cash: {
-    type: Number,
-    min: 0,
-    default: 100000,
-    required: true,
+  {
+    toJSON: {
+      // This function returns a JSON without id, __v
+      transform: (doc, ret) => {
+        delete ret.id;
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+      },
+    },
   },
-  holdings: {
-    type: Number,
-    min: 0,
-    required: true,
-  },
-});
+);
 
 const modelAccount = model<Account>('Account', AccountSchema);
 export default modelAccount;
