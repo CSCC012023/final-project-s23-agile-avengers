a2efdf944cf3ef2e39fda3e4ec43ea85ce0504b7
ShawnGeorge03 <32755895+ShawnGeorge03@users.noreply.github.com>
2023-07-26 17:13:17 -0400

    feat: 🌱 updated seed file for unit
    FIN-127

--- server/src/config/seed.ts
+++ server/src/config/seed.ts
@@ -12,7 +12,7 @@ import modelProgress from '../models/Learning/progress';
 import modelUnit from '../models/Learning/unit';
 import modelVideo from '../models/Learning/video';
 
-import { Article, Video } from '../types/learning';
+import { Article, Unit, Video } from '../types/learning';
 
 type MockArticle = {
   title: string;
@@ -30,6 +30,11 @@ type MockVideo = {
   description: string;
 };
 
+type MockUnit = {
+  name: string;
+  content: string[];
+};
+
 const slugifyConfig = {
   replacement: '-', // replace spaces with replacement character, defaults to `-`
   remove: undefined, // remove characters that match regex, defaults to `undefined`
@@ -39,21 +44,22 @@ const slugifyConfig = {
   trim: true, // trim leading and trailing replacement chars, defaults to `true`
 };
 
-const contentSlugs: string[] = [];
+let contentID: Record<string, string> = {};
+let contentSlugs: string[] = [];
 
 const convertDateStrToDate = (dateStr: string) => {
   const [day, month, year] = dateStr.split('/').map(Number);
   return new Date(+year, month - 1, +day);
 };
 
-const generateSlug = (input: string, type: 'article' | 'video') => {
-  let slug = `${type}/${slugify(input, slugifyConfig)}`;
+const generateSlug = (input: string) => {
+  let slug = slugify(input, slugifyConfig);
   while (contentSlugs.includes(slug))
-    slug = `${type}/${slugify(input, slugifyConfig)}-${randomBytes(20)
+    slug = `${slugify(input, slugifyConfig)}-${randomBytes(20)
       .toString('hex')
       .slice(0, 5)}`;
   contentSlugs.push(slug);
-  return slug.split('/')[1];
+  return slug;
 };
 
 const clearDB = async () => {
@@ -81,7 +87,7 @@ const seedDB = async () => {
     ({ title, createdAt, image, author, text }) => {
       return {
         title: title.trim(),
-        slug: generateSlug(title, 'article'),
+        slug: generateSlug(title),
         createdAt: convertDateStrToDate(createdAt),
         image: image.trim(),
         author: author.trim(),
@@ -93,6 +99,10 @@ const seedDB = async () => {
   await modelArticle
     .insertMany(processedArticles)
     .then((result: Article[]) => {
+      contentSlugs = [];
+      result.map(({ title, _id }) => {
+        contentID[`article/${title}`] = _id.toString();
+      });
       console.info('Articles Seeded! ✅');
     })
     .catch((err) => {
@@ -106,7 +116,7 @@ const seedDB = async () => {
     ({ name, createdAt, videoID, author, description }) => {
       return {
         name: name.trim(),
-        slug: generateSlug(name, 'video'),
+        slug: generateSlug(name),
         createdAt: convertDateStrToDate(createdAt),
         videoID: videoID.trim(),
         author: author.trim(),
@@ -118,7 +128,40 @@ const seedDB = async () => {
   await modelVideo
     .insertMany(processedVideos)
     .then((result: Video[]) => {
-      console.info('Articles Seeded! ✅');
+      contentSlugs = [];
+      result.map(({ name, _id }) => {
+        contentID[`video/${name}`] = _id.toString();
+      });
+      console.info('Videos Seeded! ✅');
+    })
+    .catch((err) => {
+      console.error(err);
+      process.exit();
+    });
+
+  console.info('Seeding Units...');
+  const mockUnits: MockUnit[] = require('../mock/units.json');
+  const processedUnits = mockUnits.map(({ name, content }) => {
+    const unitContent: string[] = [];
+    for (const [key, value] of Object.entries(contentID))
+      if (content.includes(key)) unitContent.push(value);
+
+    return {
+      name: name.trim(),
+      slug: generateSlug(name),
+      content: unitContent,
+    };
+  });
+
+  await modelUnit
+    .insertMany(processedUnits)
+    .then((result: Unit[]) => {
+      contentSlugs = [];
+      contentID = {};
+      result.map(({ name, _id }) => {
+        contentID[`unit/${name}`] = _id.toString();
+      });
+      console.info('Units Seeded! ✅');
     })
     .catch((err) => {
       console.error(err);
