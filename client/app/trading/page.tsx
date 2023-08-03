9f42926f5585c7ba2fd55dcebb11eb9da19b80f2
Adam Badar <adam.badar@mail.utoronto.ca>
2023-08-03 18:43:01 -0400

    feat: ✨ implemented show max when buying a stock

--- client/app/trading/page.tsx
+++ client/app/trading/page.tsx
@@ -32,6 +32,7 @@ import {
 import { useState } from 'react';
 
 import SymbolSearch from '@/components/SymbolSearch';
+import { useAuth } from '@clerk/nextjs';
 
 export default function TradingPage() {
   const { isOpen: isSearchOpen, onOpen, onClose } = useDisclosure();
@@ -42,7 +43,43 @@ export default function TradingPage() {
   } = useDisclosure();
   const [action, setAction] = useState('Buy');
   const [amount, setAmount] = useState('');
+  const { userId } = useAuth();
   const [symbol, setSymbol] = useState('');
+  const [selectedAction, setSelectedAction] = useState<string>('Buy');
+  const [price, setPrice] = useState<number | null>(null);
+  const [maxAmount, setMaxAmount] = useState<number | null>(0);
+
+  const getPrice = async () => {
+    if (!userId) return;
+    try {
+      const response: Response = await fetch(
+        `http://localhost:4000/latestPrice?symbol=${symbol}`,
+      );
+      if (!response.ok) throw new Error('Response not ok');
+      const data = await response.json();
+      const price: number = parseFloat(data.price);
+      setPrice(price);
+      return price;
+    } catch (e: any) {
+      console.error(e);
+    }
+  };
+
+  const calculateMaxAmount = () => {
+    if (selectedAction === 'Buy' && price)
+      setMaxAmount(Math.floor(100000 / price));
+    //else if (selectedAction === 'Sell') setMaxAmount(floor(await getShares()));
+    else setMaxAmount(null);
+  };
+
+  const handleActionChange = (e: any) => {
+    setSelectedAction(e.target.value);
+  };
+
+  const handleMaxClick = () => {
+    getPrice();
+    calculateMaxAmount();
+  };
 
   const handleAmountChange = (e: any) => {
     setAmount(e.target.value);
@@ -93,9 +130,7 @@ export default function TradingPage() {
             width={'30%'}>
             <FormLabel>Action</FormLabel>
             <Select
-              onChange={handleActionChange}
-              placeholder="Buy"
-              value={action}
+              placeholder="Select"
               width={'100%'}>
               <option>Sell</option>
             </Select>
@@ -112,7 +147,6 @@ export default function TradingPage() {
               width={'50%'}>
               <FormLabel>Amount</FormLabel>
               <Input
-                onChange={handleAmountChange}
                 placeholder="0"
                 type="number"
                 value={amount}
@@ -128,9 +162,9 @@ export default function TradingPage() {
             <Button
               alignSelf={'flex-end'}
               colorScheme="gray"
-              isDisabled={symbol === ''}
               minWidth="100px"
               mt={4}
+              onClick={handleMaxClick}
               p={5}>
               <ViewIcon mr={2}></ViewIcon>Show max
             </Button>
@@ -145,8 +179,12 @@ export default function TradingPage() {
             width={'30%'}>
             <FormLabel>Order Type</FormLabel>
             <Select
-              placeholder="Market"
-              width={'100%'}></Select>
+              placeholder="Select"
+              width={'100%'}>
+              <option>Market</option>
+              <option>Edit this</option>
+              <option>Edit this</option>
+            </Select>
           </FormControl>
 
           <FormControl
@@ -155,8 +193,12 @@ export default function TradingPage() {
             width={'30%'}>
             <FormLabel>Duration</FormLabel>
             <Select
-              placeholder="Day Only"
-              width={'100%'}></Select>
+              placeholder="Select"
+              width={'100%'}>
+              <option>Day Only</option>
+              <option>Night Only</option>
+              <option>Edit this</option>
+            </Select>
           </FormControl>
         </Flex>
 
