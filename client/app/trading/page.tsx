d356116a33a1b3906f4fbfb769db93283aea14ee
ShawnGeorge03 <32755895+ShawnGeorge03@users.noreply.github.com>
2023-08-03 13:07:31 -0400

    feat: ✨ updated Trading Page UI with Account Info
    FIN-128

--- client/app/trading/page.tsx
+++ client/app/trading/page.tsx
@@ -8,20 +8,36 @@ import {
   Heading,
   Input,
   Select,
+  Text,
 } from '@chakra-ui/react';
 import { useState } from 'react';
+import { CopyrightStyles, MiniChart } from 'react-ts-tradingview-widgets';
 
 import SymbolSearch from '@/components/SymbolSearch';
+import useWindowWidth from '@/hooks/useWindowWidth';
 
 import styles from '@/styles/pages/Trading.module.scss';
+
+const USDollar = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+});
+
+const tradingViewStyles: CopyrightStyles = { parent: { display: 'none' } };
 
 export default function TradingPage() {
   const [symbol, setSymbol] = useState('');
 
   const {
     container,
-    tradeWrapper,
+    accountInfo,
+    info,
+    valueName,
+    valueText,
     searchWrapper,
+    tradeWrapper,
+    symbolInfo,
+    tradeContainer,
     optionsWrapper,
     orderWrapper,
     amountsWrapper,
@@ -29,6 +45,11 @@ export default function TradingPage() {
     showMax,
     buttonWrapper,
   } = styles;
+
+  const cash = 100000;
+  const value = 100192.58;
+
+  const windowWidth = useWindowWidth();
 
   return (
     <div className={container}>
@@ -39,62 +60,88 @@ export default function TradingPage() {
       </Heading>
       <div className={searchWrapper}>
         <SymbolSearch callback={(symbol: string) => setSymbol(symbol)} />
+        <div className={accountInfo}>
+          <Text className={`${info} ${valueName}`}> Account Value </Text>
+          <Text className={`${info} ${valueName}`}>Cash</Text>
+          <Text className={`${info} ${valueText}`}>
+            {USDollar.format(value)}
+          </Text>
+          <Text className={`${info} ${valueText}`}>
+            {USDollar.format(cash)}
+          </Text>
+        </div>
       </div>
       <div className={tradeWrapper}>
-        <div className={optionsWrapper}>
-          <FormControl className={orderWrapper}>
-            <FormLabel>Order Type</FormLabel>
-            <Select
-              placeholder="Select"
-              width="100%">
-              <option>Market</option>
-            </Select>
-          </FormControl>
-
-          <FormControl>
-            <FormLabel>Action</FormLabel>
-            <Select
-              placeholder="Select"
-              width="100%">
-              <option>Buy</option>
-              <option>Sell</option>
-            </Select>
-          </FormControl>
-        </div>
-
-        <div className={amountsWrapper}>
-          <FormControl
-            className={amountContainer}
-            mr="10%">
-            <FormLabel>Amount</FormLabel>
-            <Input
-              min={0}
-              type="number"
+        <div className={symbolInfo}>
+          {symbol && (
+            <MiniChart
+              autosize={windowWidth <= 790}
+              colorTheme="light"
+              copyrightStyles={tradingViewStyles}
+              dateRange="1M"
+              height="270"
+              largeChartUrl={`http://localhost:3000/research/info`}
+              symbol={symbol}
+              width="560"
             />
-          </FormControl>
-
-          <Button
-            className={showMax}
-            colorScheme="gray"
-            maxWidth="30%">
-            Show Max
-          </Button>
+          )}
         </div>
+        <div className={tradeContainer}>
+          <div className={optionsWrapper}>
+            <FormControl className={orderWrapper}>
+              <FormLabel>Order Type</FormLabel>
+              <Select
+                placeholder="Select"
+                width="100%">
+                <option>Market</option>
+              </Select>
+            </FormControl>
 
-        <ButtonGroup
-          className={buttonWrapper}
-          spacing="25">
-          <Button
-            colorScheme="red"
-            size="lg">
-            Cancel
-          </Button>
-          <Button
-            colorScheme="blue"
-            size="lg">
-            Preview Order
-          </Button>
-        </ButtonGroup>
+            <FormControl>
+              <FormLabel>Action</FormLabel>
+              <Select
+                placeholder="Select"
+                width="100%">
+                <option>Buy</option>
+                <option>Sell</option>
+              </Select>
+            </FormControl>
+          </div>
+
+          <div className={amountsWrapper}>
+            <FormControl
+              className={amountContainer}
+              mr="10%">
+              <FormLabel>Amount</FormLabel>
+              <Input
+                min={0}
+                type="number"
+              />
+            </FormControl>
+
+            <Button
+              className={showMax}
+              colorScheme="gray"
+              maxWidth="30%">
+              Show Max
+            </Button>
+          </div>
+
+          <ButtonGroup
+            className={buttonWrapper}
+            spacing="25">
+            <Button
+              colorScheme="red"
+              size="lg">
+              Cancel
+            </Button>
+            <Button
+              colorScheme="blue"
+              size="lg">
+              Preview Order
+            </Button>
+          </ButtonGroup>
+        </div>
       </div>
     </div>
   );
