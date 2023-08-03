7728a73538ac68b4cf14a49dfde7aa32a354d6e9
ShawnGeorge03 <32755895+ShawnGeorge03@users.noreply.github.com>
2023-08-03 16:09:06 -0400

    fix: 🌱 bug in `seedTradingDB`

--- server/src/config/seed/trading.ts
+++ server/src/config/seed/trading.ts
@@ -1,3 +1,5 @@
+import { exit } from 'process';
+
 import modelUser from '../../models/Account/user';
 import modelAccount from '../../models/Trading/account';
 import modelPortfolio from '../../models/Trading/portfolio';
@@ -15,31 +17,80 @@ export const seedTradingDB = async () => {
   const users = await modelUser.find().select('_id');
 
   console.info('Seeding Trading...');
-  users.flatMap(async (user) => {
-    const stockPrice = Math.random() * (192.37 - 190.69) + 190.69;
-    const numOwned = Math.floor(Math.random() * 9) + 1;
-    const cash = 100000 - 191.38 * numOwned;
-    await modelAccount.create({
+
+  const portfolios = users.flatMap((user) => {
+    return {
       userID: user._id,
-      cash,
-      value: cash + (stockPrice - 191.38) * numOwned,
-      holdings: {
-        equity: {
-          AAPL: numOwned,
+      equity: [
+        {
+          date: Date.now(),
+          action: 'buy',
+          order: 'market',
+          symbol: 'MSFT',
+          quantity: Math.floor(Math.random() * 9) + 1,
+          price: 326.66,
         },
-      },
+        {
+          date: Date.now(),
+          action: 'buy',
+          order: 'market',
+          symbol: 'AAPL',
+          quantity: Math.floor(Math.random() * 9) + 1,
+          price: 191.38,
+        },
+      ],
+    };
+  });
+
+  await modelPortfolio
+    .insertMany(portfolios)
+    .then(() => {
+      console.info('Portfolios Seeded! ✅');
+    })
+    .catch((err) => {
+      console.error(err);
+      exit(1);
     });
 
-    await modelPortfolio.create({
-      userID: user._id,
-      equity: {
-        date: Date.now(),
-        action: 'buy',
-        order: 'market',
-        symbol: 'AAPL',
-        quantity: numOwned,
-        price: 191.38,
+  const accounts = portfolios.flatMap(({ userID, equity }) => {
+    const currAAPLPrice = Math.random() * (192.37 - 190.69) + 190.69;
+    const currMSFTPrice = Math.random() * (329.88 - 325.95) + 325.95;
+
+    const AAPLOwned = equity.find((value) => value.symbol === 'AAPL');
+    const MSFTOwned = equity.find((value) => value.symbol === 'MSFT');
+
+    const cash =
+      100000 -
+      ((AAPLOwned?.price || 191.38) * (AAPLOwned?.quantity || 1) +
+        (MSFTOwned?.price || 326.66) * (MSFTOwned?.quantity || 1));
+
+    const value =
+      cash +
+      (currAAPLPrice - (AAPLOwned?.price || 191.38)) *
+        (AAPLOwned?.quantity || 1) +
+      (currMSFTPrice - (MSFTOwned?.price || 326.66)) *
+        (MSFTOwned?.quantity || 1);
+
+    return {
+      userID,
+      cash,
+      value,
+      holdings: {
+        equity: {
+          AAPL: AAPLOwned?.quantity || 1,
+          MSFT: MSFTOwned?.quantity || 1,
+        },
       },
-    });
+    };
   });
+
+  await modelAccount
+    .insertMany(accounts)
+    .then(() => {
+      console.info('Accounts Seeded! ✅');
+    })
+    .catch((err) => {
+      console.error(err);
+      exit(1);
+    });
 };
